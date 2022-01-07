484c0ab9ecfdb76c1db93fd9d45abe98cb1f3e41
Deepika Palthode <96534060+deepikapalthode@users.noreply.github.com>
2022-01-08 04:29:07 +0530

    Add files via upload

---
+++ controlStructures.R
@@ -0,0 +1,41 @@
+temp <- 25
+if(temp<20)
+{print("cool day")
+  } else if (temp>20 & temp<=30)
+  {print("good day")
+}else if(temp>30 & temp<=40)
+print("hotday")
+
+v <- switch(x,"a"="apple","b"="banana","c"="cherry")
+x="a"
+v
+v=switch(1,"apple","berry")
+v
+v=switch("b",a="avocado","b"="berry")
+v
+cu <- c("rupee","cad","scd","nu","usd")
+for(i in cu)
+  print(i)
+c=0
+while(c<=10)
+{c=c+2
+  print(c)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
